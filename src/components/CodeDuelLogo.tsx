6e08f575876373ed689208c5bddc8e102a451642
Nischal Yerramsetty <bt23csa015@iiitn.ac.in>
2025-08-31 14:26:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CodeDuelLogo>
    
    Re

--- src/components/CodeDuelLogo.tsx
+++ src/components/CodeDuelLogo.tsx
@@ -9,15 +9,19 @@ export function CodeDuelLogo(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="hsl(var(--primary))"
-      strokeWidth="2"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       aria-label="CodeDuelz Logo"
       {...props}
     >
-      <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" />
-      <path d="M10 9L8 12L10 15" />
-      <path d="M14 9L16 12L14 15" />
+      <path d="M12 2a5 5 0 0 0-5 5v2.5" />
+      <path d="M17 9.5V7a5 5 0 0 0-10 0v2.5" />
+      <path d="M12 22V12" />
+      <path d="M9 12H7.5a4.5 4.5 0 1 1 0-9" />
+      <path d="M15 12h1.5a4.5 4.5 0 1 0 0-9" />
+      <path d="M9 16l-4-4" />
+      <path d="M15 16l4-4" />
     </svg>
   );
 }
