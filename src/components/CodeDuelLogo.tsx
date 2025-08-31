c3b521baed3a4a3548b85143086deead71d97fdc
Nischal Yerramsetty <bt23csa015@iiitn.ac.in>
2025-08-31 14:39:40 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CodeDuelLogo>
    
    Re

--- src/components/CodeDuelLogo.tsx
+++ src/components/CodeDuelLogo.tsx
@@ -3,8 +3,8 @@ import type { SVGProps } from 'react';
 export function CodeDuelLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width="32"
-      height="32"
+      width="40"
+      height="40"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
